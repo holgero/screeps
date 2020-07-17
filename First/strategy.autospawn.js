aff6621dc9fd075e3f1f2f490a7e76bd55681492
Holger Oehm <holger.oehm@holger-oehm.de>
2020-07-17 10:42:40 +0200

    Build bigger creeps

--- First/strategy.autospawn.js
+++ First/strategy.autospawn.js
@@ -12,13 +12,35 @@ var strategySpawn = {
             return 0;
         } else {
             if (creeps.length < desiredCreeps) {
-                var err = spawn.spawnCreep([WORK,CARRY,MOVE], 'whatever', {memory: {role: role}, dryRun: true});
+                var body;
+                if (spawn.room.controller.level < 2) {
+                    body = [WORK, CARRY, MOVE];
+                } else {
+                    body = [WORK, CARRY, CARRY, MOVE, MOVE];
+                }
+                switch (spawn.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }}).length) {
+                    case 0:
+                        break;
+                    case 1:
+                        body.push(MOVE);
+                        break;
+                    case 2:
+                        body.push(MOVE, CARRY);
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                    default:
+                        body.push(MOVE, CARRY, MOVE);
+                        break;
+                }
+                var err = spawn.spawnCreep(body, 'whatever', {memory: {role: role}, dryRun: true});
                 if (!err) {
                     var newName = _.capitalize(role) + Game.time;
                     console.log('Spawning new ' + role + ': ' + newName);
-                    spawn.spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: role}});
-                } else if (err != ERR_NOT_ENOUGH_ENERGY) {
-                    console.log('Cant spawn a new creep: ' + err);
+                    spawn.spawnCreep(body, newName, {memory: {role: role}});
+                } else {
+                    console.log('Cant spawn a new creep with body ' + body + ': ' + err);
                 }
                 return 0;
             }
