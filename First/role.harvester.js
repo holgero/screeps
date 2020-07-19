60935c6bdf62cf9a9dafa70880e2b8627511c3bb
Holger Oehm <holger.oehm@holger-oehm.de>
2020-07-19 19:59:33 +0200

    Fill up spawn and extensions before tower

--- First/role.harvester.js
+++ First/role.harvester.js
@@ -23,14 +23,25 @@ var roleHarvester = {
 	    }
 
 	    if (creep.memory.feeding) {
-            var targets = creep.room.find(FIND_STRUCTURES, {
+            var targets = creep.room.find(FIND_MY_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION
                                 || structure.structureType == STRUCTURE_SPAWN
-                                 || structure.structureType == STRUCTURE_TOWER) &&
+                                || structure.structureType == STRUCTURE_TOWER
+                                || structure.structureType == STRUCTURE_STORAGE) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
+            function ordinalOf(target) {
+                switch (target.structureType) {
+                    case STRUCTURE_SPAWN: return 1;
+                    case STRUCTURE_EXTENSION: return 2;
+                    case STRUCTURE_TOWER: return 3;
+                    case STRUCTURE_STORAGE: return 4;
+                    default: return 5;
+                }
+            };
+            targets.sort((a,b) => ordinalOf(a) - ordinalOf(b));
             if (targets.length > 0) {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
