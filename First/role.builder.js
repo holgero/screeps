cc38c27158e4ecf3e5250e1aa09027a7180a653d
Holger Oehm <holger.oehm@holger-oehm.de>
2020-07-19 21:03:38 +0200

    Move to suitable place from distance 4 to target

--- First/role.builder.js
+++ First/role.builder.js
@@ -28,6 +28,37 @@ var roleBuilder = {
         }
         return false;
     },
+    findSuitablePlace: function(creep, target, sources) {
+        var room = creep.room;
+        var top = Math.max(0, target.pos.y-3);
+        var left = Math.max(0, target.pos.x-3);
+        var bottom = Math.min(49, target.pos.y+3);
+        var right = Math.min(target.pos.x+3);
+        var areaStuff = room.lookAtArea(top, left, bottom, right);
+        // console.log('All around target: ' + JSON.stringify(areaStuff));
+        for (var y=top; y<=bottom; y++) {
+            for (var x=left; x<=right; x++) {
+                if (target.pos.isEqualTo(x, y)) {
+                    continue;
+                }
+                if (creep.pos.isEqualTo(x, y)) {
+                    continue;
+                }
+                var currentRow = areaStuff[y][x];
+                // console.log(x, y, JSON.stringify(currentRow));
+                if (_.filter(currentRow, roleBuilder.cannotParkOn).length > 0) {
+                    continue;
+                }
+                for (var source of sources) {
+                    if (source.pos.isNearTo(x,y)) {
+                        continue;
+                    }
+                }
+                return creep.room.getPositionAt(x,y);
+            }
+        }
+        return null;
+    },
     /** @param {Creep} creep **/
     run: function(creep) {
         commons.releaseEnergySources(creep);
@@ -46,44 +77,28 @@ var roleBuilder = {
 	        var targets = room.find(FIND_CONSTRUCTION_SITES);
             if (targets.length) {
                 var target = targets[0];
-                if (creep.pos.inRangeTo(target.pos, 3)) {
+                if (creep.pos.inRangeTo(target.pos, 4)) {
                     var sources = room.find(FIND_SOURCES);
-                    if (roleBuilder.noParking(room, sources, creep.pos)) {
-                        var top = Math.max(0, target.pos.y-3);
-                        var left = Math.max(0, target.pos.x-3);
-                        var bottom = Math.min(49, target.pos.y+3);
-                        var right = Math.min(target.pos.x+3);
-                        var areaStuff = room.lookAtArea(top, left, bottom, right);
-                        // console.log('All around target: ' + JSON.stringify(areaStuff));
-                        for (var y=top; y<=bottom; y++) {
-                            for (var x=left; x<=right; x++) {
-                                if (target.pos.isEqualTo(x, y)) {
-                                    continue;
-                                }
-                                if (creep.pos.isEqualTo(x, y)) {
-                                    continue;
-                                }
-                                var currentRow = areaStuff[y][x];
-                                // console.log(x, y, JSON.stringify(currentRow));
-                                if (_.filter(currentRow, roleBuilder.cannotParkOn).length > 0) {
-                                    continue;
-                                }
-                                for (var source of sources) {
-                                    if (source.pos.isNearTo(x,y)) {
-                                        continue;
-                                    }
-                                }
-                                // console.log('Found a suitable place at (' + x + ',' + y + '): ' + JSON.stringify(currentRow));
-                                creep.moveTo(x, y, { visualizePathStyle: {stroke: '#ffffff'}, ignoreRoads: true });
+                    if (creep.pos.inRangeTo(target.pos, 3)) {
+                        if (roleBuilder.noParking(room, sources, creep.pos)) {
+                            var place = roleBuilder.findSuitablePlace(creep, target, sources);
+                            if (place != null) {
+                                creep.moveTo(place, { visualizePathStyle: {stroke: '#ffffff'}, ignoreRoads: true });
                                 return;
                             }
+                        } else {
+                            var err = creep.build(target);
+                            if (err != OK) {
+                               console.log('Build failed: ' + err);
+                            }
+                            return;
                         }
                     } else {
-                        var err = creep.build(target);
-                        if (err != OK) {
-                           console.log('Build failed: ' + err);
+                        var place = roleBuilder.findSuitablePlace(creep, target, sources);
+                        if (place != null) {
+                            creep.moveTo(place, { visualizePathStyle: {stroke: '#ffffff'}, ignoreRoads: true });
+                            return;
                         }
-                        return;
                     }
                 }
                 creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
