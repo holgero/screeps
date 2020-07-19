de97799fb3d0790ac2a90c45a14c04944bca7517
Holger Oehm <holger.oehm@holger-oehm.de>
2020-07-19 13:06:33 +0200

    Rework no parking logic
    More reasons not to park at a spot (roads, containers, walls).
    Find a suitable spot.
    Don't park on spots near to sources.

--- First/role.builder.js
+++ First/role.builder.js
@@ -8,7 +8,27 @@ var roleBuilder = {
         increaseMove: true,
         increaseWork: true,
     },
-
+    cannotParkOn: function(o) {
+        return o.type == LOOK_FLAGS && o.flag.name.startsWith('no parking') ||
+            o.type == LOOK_STRUCTURES && o.structure.structureType == STRUCTURE_CONTAINER ||
+            o.type == LOOK_STRUCTURES && o.structure.structureType == STRUCTURE_ROAD ||
+            o.type == LOOK_TERRAIN && o.terrain == "wall";
+    },
+    noParking: function(room, pos) {
+        var stuff = room.lookAt(pos);
+        var filtered = _.filter(stuff, roleBuilder.cannotParkOn);
+        // console.log(JSON.stringify(filtered));
+        if (filtered.length>0) {
+            return true;
+        }
+        var sources = room.find(FIND_SOURCES);
+        for (var source of sources) {
+            if (source.pos.isNearTo(pos)) {
+                return true;
+            }
+        }
+        return false;
+    },
     /** @param {Creep} creep **/
     run: function(creep) {
         commons.releaseEnergySources(creep);
@@ -28,9 +48,29 @@ var roleBuilder = {
             if (targets.length) {
                 var target = targets[0];
                 if (creep.pos.inRangeTo(target.pos, 3)) {
-                    var flags = _.filter(room.lookForAt(LOOK_FLAGS, creep.pos), function(flag) { return flag.name.startsWith('no parking'); });
-                    if (flags.length > 0) {
-                        // console.log(JSON.stringify(flags));
+                    if (roleBuilder.noParking(room, creep.pos)) {
+                        var top = Math.max(0, target.pos.y-3);
+                        var left = Math.max(0, target.pos.x-3);
+                        var bottom = Math.min(49, target.pos.y+3);
+                        var right = Math.min(target.pos.x+3);
+                        var areaStuff = room.lookAtArea(top, left, bottom, right);
+                        // console.log('All around target: ' + JSON.stringify(areaStuff));
+                        for (var y=top; y<=bottom; y++) {
+                            for (var x=left; x<=right; x++) {
+                                if (target.pos.isNearTo(x, y)) {
+                                    // skip inner circle
+                                    continue;
+                                }
+                                var currentRow = areaStuff[y][x];
+                                // console.log(x, y, JSON.stringify(currentRow));
+                                if (_.filter(currentRow, roleBuilder.cannotParkOn).length > 0) {
+                                    continue;
+                                }
+                                // console.log('Found a suitable place at (' + x + ',' + y + '): ' + JSON.stringify(currentRow));
+                                creep.moveTo(x, y);
+                                return;
+                            }
+                        }
                     } else {
                         var err = creep.build(target);
                         if (err != OK) {
