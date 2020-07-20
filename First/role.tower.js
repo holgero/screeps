10398f4600376eeaa9b6f93aab37b599b9869130
Holger Oehm <holger.oehm@holger-oehm.de>
2020-07-20 18:40:36 +0200

    Use up tower energy slower

--- First/role.tower.js
+++ First/role.tower.js
@@ -3,9 +3,12 @@ var roleTower = {
         var towers = room.find(FIND_MY_STRUCTURES, { filter: {structureType: STRUCTURE_TOWER}});
         if (towers.length) {
             towers.forEach(function(tower) {
-                var maxi = [100, 1000, 10000, 20000, 50000, 100000, 1000000];
+                var maxi = [100, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 1000000];
                 for (var i=0;i<maxi.length;i++) {
                     if (10*tower.store[RESOURCE_ENERGY]/tower.store.getCapacity(RESOURCE_ENERGY)<i) {
+                        break;
+                    }
+                    if (i>=5 && Game.time % 20 !=0) {
                         break;
                     }
                     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
