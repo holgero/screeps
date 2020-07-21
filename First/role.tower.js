94530343cb6435b5bc2a12f3d0d22c4354c4acb2
Holger Oehm <holger.oehm@holger-oehm.de>
2020-07-21 09:43:30 +0200

    Run tower repairs in bursts

--- First/role.tower.js
+++ First/role.tower.js
@@ -3,19 +3,18 @@ var roleTower = {
         var towers = room.find(FIND_MY_STRUCTURES, { filter: {structureType: STRUCTURE_TOWER}});
         if (towers.length) {
             towers.forEach(function(tower) {
-                var maxi = [100, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 1000000];
-                for (var i=0;i<maxi.length;i++) {
-                    if (10*tower.store[RESOURCE_ENERGY]/tower.store.getCapacity(RESOURCE_ENERGY)<i) {
-                        break;
-                    }
-                    if (i>=5 && Game.time % 5 !=0) {
-                        break;
-                    }
-                    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                        filter: (structure) => structure.hits < structure.hitsMax && structure.hits < maxi[i] });
-                    if (closestDamagedStructure) {
-                        tower.repair(closestDamagedStructure);
-                        break;
+                if (Game.time % 100 < 50) {
+                    var maxi = [100, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 1000000];
+                    for (var i=0;i<maxi.length;i++) {
+                        if (10*tower.store[RESOURCE_ENERGY]/tower.store.getCapacity(RESOURCE_ENERGY)<i) {
+                            break;
+                        }
+                        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                            filter: (structure) => structure.hits < structure.hitsMax && structure.hits < maxi[i] });
+                        if (closestDamagedStructure) {
+                            tower.repair(closestDamagedStructure);
+                            break;
+                        }
                     }
                 }
                 var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
