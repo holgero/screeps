b8497c63aafc3f4ba858026fcdc7e254ef1b31ed
Holger Oehm <holger.oehm@holger-oehm.de>
2020-08-16 22:19:57 +0200

    Use common findFeedTarget

--- First/role.harvester.js
+++ First/role.harvester.js
@@ -31,34 +31,18 @@ var roleHarvester = {
 	    }
 
 	    if (creep.memory.feeding) {
-            var targets = creep.room.find(FIND_MY_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION
-                                || structure.structureType == STRUCTURE_SPAWN
-                                || structure.structureType == STRUCTURE_TOWER
-                                || structure.structureType == STRUCTURE_STORAGE) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-            function ordinalOf(target) {
-                switch (target.structureType) {
-                    case STRUCTURE_SPAWN: return 1;
-                    case STRUCTURE_TOWER: return 2;
-                    case STRUCTURE_EXTENSION: return 3;
-                    case STRUCTURE_STORAGE: return 4;
-                    default: return 5;
-                }
-            };
-            targets.sort((a,b) => ordinalOf(a) - ordinalOf(b));
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    commons.moveTo(creep, targets[0].pos);
-                } else {
+            if (!creep.memory.feedTarget) {
+                commons.findFeedTarget(creep);
+            }
+            if (creep.memory.feedTarget) {
+                const target = Game.getObjectById(creep.memory.feedTarget);
+                var err = creep.transfer(target, RESOURCE_ENERGY);
+                if (err == ERR_NOT_IN_RANGE) {
+                    commons.moveTo(creep, target.pos);
+                } else if (err == OK || err == ERR_FULL) {
+                    delete creep.memory.feedTarget;
                     delete creep.memory.movePath;
                 }
-            } else {
-                creep.say('park');
-                commons.gotoSpawn(creep, creep.room);
             }
         } else {
             commons.fetchEnergy(creep, true, false);
